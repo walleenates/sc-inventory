cc609ea87369b1b9f7c6209a2a81133213487cde
Walleen.Ates <walleenates.202100119@gmail.com>
2024-09-10 15:45:21 +0800

    layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
       <nav className="sidebar-nav">
         <Link to="/dashboard" className="nav-link">Dashboard</Link>
         <Link to="/manage-item" className="nav-link">Manage Item</Link>
-        <Link to="/approve-request" className="nav-link">Approve Purchased Request</Link>
+        <Link to="/approve-request" className="nav-link">Approved Purchased Request</Link>
         <Link to="/reports" className="nav-link">Reports</Link>
         <Link to="/scanner" className="nav-link">Scanner</Link>
         <Link to="/settings" className="nav-link">Settings</Link>
